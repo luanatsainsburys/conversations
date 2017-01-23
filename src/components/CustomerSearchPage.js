5ae66dabdb1675d358c0cecdc9761ae29954a1bc
luanatsainsburys <luan.au@sainsburys.co.uk>
2017-01-23 10:25:50 +0000

    Refactor render function to use more composition

--- src/components/CustomerSearchPage.js
+++ src/components/CustomerSearchPage.js
@@ -1,23 +1,34 @@
 import Immutable from 'immutable';
 import React, {PropTypes} from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router';
+
 
 // import {Link} from 'react-router';
 import CustomerSearchForm from './CustomerSearchForm';
 
 const CustomerSearchPage = (props) => {
+    let errorMessage = !props.foundCustomer.get('success') && props.foundCustomer.get('errorMessage') &&
+                <div className="row">
+                    <div className="col-sm-offset-4">
+                        <span className="error">{props.foundCustomer.get('errorMessage')}</span>
+                    </div>
+                </div>;
+
+    let customerLink = props.foundCustomer.get('success') &&
+                <div className="row">
+                    <div className="col-sm-offset-4">
+                        <span>We have found this customer. Please select <Link to="/customer-search">this link to view</Link></span>
+                    </div>
+                </div>;
+
     return (
         <div>
             <div className="row">
                 <CustomerSearchForm/>
             </div>
-            {!props.foundCustomer.get('success') && props.foundCustomer.get('errorMessage') &&
-                <div className="row">
-                    <div className="col-sm-offset-4">
-                        <span className="error">{props.foundCustomer.get('errorMessage')}</span>
-                    </div>
-                </div>
-            }
+            {errorMessage}
+            {customerLink}
         </div>
         );
     };
